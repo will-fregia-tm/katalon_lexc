9910e14eb80328b8eeff1a6b66e1a1aa0d9a5677
amcgu <amcgu@192.168.1.4>
2020-04-23 11:48:51 -0700

    initial template share

---
+++ Object Repository/GlobalNav/lexus logo.rs
@@ -0,0 +1,22 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>lexus logo</name>
+   <tag></tag>
+   <elementGuidId>5845ad91-5864-47c7-b0e2-9156e17ae1de</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>BASIC</key>
+         <value>//*[@id = 'logo']</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>BASIC</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>id</name>
+      <type>Main</type>
+      <value>logo</value>
+   </webElementProperties>
+</WebElementEntity>
---
+++ Test Suites/Lexus.com/GlobalNav/Header.ts
@@ -0,0 +1,37 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Header</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>20</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>5043f7cb-4dfc-4c32-983c-e7beb4cd625f</testSuiteGuid>
+   <testCaseLink>
+      <guid>d4aa5bdf-55d8-4812-8ba0-ed1efc653f7c</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Lexus.com/GlobalNav/Header/logo is present</testCaseId>
+      <variableLink>
+         <testDataLinkId></testDataLinkId>
+         <type>DEFAULT</type>
+         <value></value>
+         <variableId>c0ebc493-fa00-43c1-985a-010299b39c4d</variableId>
+      </variableLink>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>3dc8cf54-4675-4c72-b789-9b23524a14bb</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>false</isRun>
+      <testCaseId>Test Cases/Lexus.com/GlobalNav/Header/logo is not present</testCaseId>
+      <variableLink>
+         <testDataLinkId></testDataLinkId>
+         <type>DEFAULT</type>
+         <value></value>
+         <variableId>3c7e665a-b2d6-46ac-bcc9-9f18dfaee789</variableId>
+      </variableLink>
+   </testCaseLink>
+</TestSuiteEntity>
