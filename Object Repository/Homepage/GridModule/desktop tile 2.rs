0ba1b950618f973c05cce9db4232cbd0af148501
amcgu <amcgu@192.168.1.4>
2020-04-24 13:59:53 -0700

    grid module tests

---
+++ Object Repository/Homepage/GridModule/desktop tile 2.rs
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>desktop tile 2</name>
+   <tag></tag>
+   <elementGuidId>f44fda22-6017-4658-85e2-398bc53ae3b8</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>XPATH</key>
+         <value>//*[contains(@class, 'component ContentGrid')]/div/section/div//*[contains(@class, 'desktop')]/div[2]/*[contains(@class, 'tile')]</value>
+      </entry>
+      <entry>
+         <key>BASIC</key>
+         <value></value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+</WebElementEntity>
---
+++ Object Repository/Homepage/GridModule/mobile tile 1.rs
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>mobile tile 1</name>
+   <tag></tag>
+   <elementGuidId>d7b030f7-bd77-460c-b80c-a1ec562a2e72</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>XPATH</key>
+         <value>//*[contains(@class, 'component ContentGrid')]/div/section/div//*[contains(@class, 'mobile')]//*[contains(@class, 'tile')]</value>
+      </entry>
+      <entry>
+         <key>BASIC</key>
+         <value></value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+</WebElementEntity>
