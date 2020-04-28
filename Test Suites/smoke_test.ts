92411c8c8d046d59274e39cfed14713786c1c179
amcgu <amcgu@DESKTOP-2P41VEE.home>
2020-04-27 17:50:27 -0700

    test suite updates

--- Test Suites/smoke_test.ts
+++ Test Suites/smoke_test.ts
@@ -1065,7 +1065,7 @@
             <profileName>prod</profileName>
             <runConfigurationId>saucelabs_edge_win10</runConfigurationId>
          </configuration>
-         <runEnabled>true</runEnabled>
+         <runEnabled>false</runEnabled>
          <testSuiteEntity>Test Suites/Lexus.com/HomePage/VehicleSelector</testSuiteEntity>
       </TestSuiteRunConfiguration>
       <TestSuiteRunConfiguration>
