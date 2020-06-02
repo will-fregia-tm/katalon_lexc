0e9ef9d319a855a1449c852a7834d6a9092aca50
amcgu <amcgu@DESKTOP-2P41VEE.home>
2020-06-02 14:31:31 -0700

    screen resolution settings

--- Test Suites/Lexus.com/GlobalNav/Header.ts
+++ Test Suites/Lexus.com/GlobalNav/Header.ts
@@ -14,7 +14,7 @@
       <guid>d4aa5bdf-55d8-4812-8ba0-ed1efc653f7c</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
-      <testCaseId>Test Cases/Lexus.com/GlobalNav/Header/logo is present</testCaseId>
+      <testCaseId>Test Cases/Lexus.com/FCV/Header/logo is present</testCaseId>
       <variableLink>
          <testDataLinkId></testDataLinkId>
          <type>DEFAULT</type>
@@ -26,7 +26,7 @@
       <guid>3dc8cf54-4675-4c72-b789-9b23524a14bb</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>false</isRun>
-      <testCaseId>Test Cases/Lexus.com/GlobalNav/Header/logo is not present</testCaseId>
+      <testCaseId>Test Cases/Lexus.com/FCV/Header/logo is not present</testCaseId>
       <variableLink>
          <testDataLinkId></testDataLinkId>
          <type>DEFAULT</type>
