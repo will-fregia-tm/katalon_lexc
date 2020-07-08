56ceabf01bbc69ca3297f27d0b5b51ebed5570a7
zsmith <zsmith@USLAXSSF9087.global.publicisgroupe.net>
2020-07-08 10:06:49 -0700

    logo css selector

--- Object Repository/GlobalNav/lexus logo.rs
+++ Object Repository/GlobalNav/lexus logo.rs
@@ -9,8 +9,12 @@
          <key>BASIC</key>
          <value>//*[@id = 'logo']</value>
       </entry>
+      <entry>
+         <key>CSS</key>
+         <value>#logo</value>
+      </entry>
    </selectorCollection>
-   <selectorMethod>BASIC</selectorMethod>
+   <selectorMethod>CSS</selectorMethod>
    <useRalativeImagePath>false</useRalativeImagePath>
    <webElementProperties>
       <isSelected>true</isSelected>
