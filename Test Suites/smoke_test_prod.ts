05f3235b98d786fa30a5b06f8cf070954115f0b8
zsmith <zsmith@USLAXSSF9087.global.publicisgroupe.net>
2020-07-27 11:30:28 -0700

    debugging

--- Test Suites/smoke_test_prod.ts
+++ Test Suites/smoke_test_prod.ts
@@ -1623,7 +1623,7 @@
             <profileName>prod</profileName>
             <runConfigurationId>saucelabs_ie11_win10</runConfigurationId>
          </configuration>
-         <runEnabled>true</runEnabled>
+         <runEnabled>false</runEnabled>
          <testSuiteEntity>Test Suites/Lexus.com/ModelPages/LCh/Subnav</testSuiteEntity>
       </TestSuiteRunConfiguration>
       <TestSuiteRunConfiguration>
