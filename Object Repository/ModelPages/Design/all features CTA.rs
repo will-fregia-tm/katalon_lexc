beb744c0f2bc3fbe50daab2611157e8fc454d024
zsmith <zsmith@USLAXSSF9087.global.publicisgroupe.net>
2020-08-14 15:50:02 -0700

    debugging

--- Object Repository/ModelPages/Design/all features CTA.rs
+++ Object Repository/ModelPages/Design/all features CTA.rs
@@ -6,25 +6,32 @@
    <elementGuidId>2c417155-8021-4e41-91a5-44d1d677509f</elementGuidId>
    <selectorCollection>
       <entry>
-         <key>CSS</key>
-         <value>#design > div:nth-child(2) > div > div > div > div > div:nth-child(2) > div > div > div</value>
+         <key>XPATH</key>
+         <value>//*[@id = 'design']//*[(contains(text(), 'ALL DESIGN') or contains(., 'ALL DESIGN'))]</value>
       </entry>
       <entry>
-         <key>XPATH</key>
-         <value>//*[contains(@class, 'App')]/div[3]/div/div/div/div[2]</value>
+         <key>CSS</key>
+         <value>#design div[href*=&quot;InfoLayer&quot;]</value>
       </entry>
       <entry>
          <key>BASIC</key>
-         <value>//*[@id = 'AllSafety']</value>
+         <value>//*[(contains(text(), 'ALL DESIGN') or contains(., 'ALL DESIGN'))]</value>
       </entry>
    </selectorCollection>
-   <selectorMethod>CSS</selectorMethod>
+   <selectorMethod>XPATH</selectorMethod>
    <useRalativeImagePath>false</useRalativeImagePath>
    <webElementProperties>
-      <isSelected>true</isSelected>
+      <isSelected>false</isSelected>
       <matchCondition>equals</matchCondition>
       <name>id</name>
       <type>Main</type>
       <value>AllSafety</value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>contains</matchCondition>
+      <name>text</name>
+      <type>Main</type>
+      <value>ALL DESIGN</value>
    </webElementProperties>
 </WebElementEntity>
--- Test Suites/smoke_test_staging.ts
+++ Test Suites/smoke_test_staging.ts
@@ -30,7 +30,7 @@
             <profileName>staging</profileName>
             <runConfigurationId>saucelabs_chrome_win10</runConfigurationId>
          </configuration>
-         <runEnabled>true</runEnabled>
+         <runEnabled>false</runEnabled>
          <testSuiteEntity>Test Suites/Lexus.com/ModelPages/RX/Gallery</testSuiteEntity>
       </TestSuiteRunConfiguration>
       <TestSuiteRunConfiguration>
@@ -120,7 +120,7 @@
             <profileName>staging</profileName>
             <runConfigurationId>saucelabs_chrome_win10</runConfigurationId>
          </configuration>
-         <runEnabled>true</runEnabled>
+         <runEnabled>false</runEnabled>
          <testSuiteEntity>Test Suites/Lexus.com/ModelPages/RX/Visualizer</testSuiteEntity>
       </TestSuiteRunConfiguration>
       <TestSuiteRunConfiguration>
@@ -147,7 +147,7 @@
             <profileName>staging</profileName>
             <runConfigurationId>saucelabs_chrome_win10</runConfigurationId>
          </configuration>
-         <runEnabled>true</runEnabled>
+         <runEnabled>false</runEnabled>
          <testSuiteEntity>Test Suites/Lexus.com/ModelPages/RXh/Gallery</testSuiteEntity>
       </TestSuiteRunConfiguration>
       <TestSuiteRunConfiguration>
@@ -237,7 +237,7 @@
             <profileName>staging</profileName>
             <runConfigurationId>saucelabs_chrome_win10</runConfigurationId>
          </configuration>
-         <runEnabled>true</runEnabled>
+         <runEnabled>false</runEnabled>
          <testSuiteEntity>Test Suites/Lexus.com/ModelPages/RXh/Visualizer</testSuiteEntity>
       </TestSuiteRunConfiguration>
       <TestSuiteRunConfiguration>
