691a0692b468a30e9e424fd521c186dc1c014e01
zsmith <zsmith@USLAXSSF9087.global.publicisgroupe.net>
2020-09-16 15:59:55 -0700

    LEXC-4528 updates

--- Scripts/Lexus.com/ModelPages/LX/Subnav/Deep link takes user to pre-determined location on page - Signup Form/Script1600217135147.groovy
+++ Scripts/Lexus.com/ModelPages/LX/Subnav/Deep link takes user to pre-determined location on page - Signup Form/Script1600217135147.groovy
@@ -15,7 +15,7 @@ import internal.GlobalVariable as GlobalVariable
 
 WebUI.openBrowser(GlobalVariable.TS_Domain + GlobalVariable.Header)
 
-WebUI.navigateToUrl((GlobalVariable.TS_Domain + GlobalVariable.Overview_LX) + '?link[ModelSignUpForm][SHOW_PAGE]=true&zip=90094')
+WebUI.navigateToUrl((GlobalVariable.TS_Domain + GlobalVariable.Overview_LX) + '?link[ModelSignUpFormLX][SHOW_PAGE]=true&zip=90094')
 
 WebUI.waitForPageLoad(0)
 
