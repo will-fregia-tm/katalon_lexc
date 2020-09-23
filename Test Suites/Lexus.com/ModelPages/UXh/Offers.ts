af726513b9da81b98c242fb3d46e1070b6f222a8
zsmith <zsmith@USLAXSSF9087.global.publicisgroupe.net>
2020-09-23 15:58:49 -0700

    initial setup

---
+++ Test Suites/Lexus.com/ModelPages/UXh/Offers.ts
@@ -0,0 +1,122 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Offers</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>20</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>0719e9f3-6ebe-4740-8968-42cd33c0e460</testSuiteGuid>
+   <testCaseLink>
+      <guid>ef164146-ed05-455d-9f8a-a1599bd412a8</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Lexus.com/ModelPages/UXh/Offers/Browse Offers - Change zip</testCaseId>
+      <variableLink>
+         <testDataLinkId></testDataLinkId>
+         <type>DEFAULT</type>
+         <value></value>
+         <variableId>ac211db6-e199-4875-9141-63664e855bc7</variableId>
+      </variableLink>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>8e5833aa-8930-4989-9939-a109cd3fda43</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Lexus.com/ModelPages/UXh/Offers/Change Zipcode - Click Change</testCaseId>
+      <variableLink>
+         <testDataLinkId></testDataLinkId>
+         <type>DEFAULT</type>
+         <value></value>
+         <variableId>33bfa78f-e94d-4fa8-80c0-acf76be77dd0</variableId>
+      </variableLink>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>1b4a657c-cd82-4cad-9c87-5269e825fd65</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Lexus.com/ModelPages/UXh/Offers/Change Zipcode - Submit new valid zip-code</testCaseId>
+      <variableLink>
+         <testDataLinkId></testDataLinkId>
+         <type>DEFAULT</type>
+         <value></value>
+         <variableId>e89912a5-6f09-4f8d-830c-d079fa0f84b4</variableId>
+      </variableLink>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>48adf8f3-a219-42ed-a0e8-b2ea596ddf01</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Lexus.com/ModelPages/UXh/Offers/Enter Zip Code - Information systems required to submit offers are not functioning</testCaseId>
+      <variableLink>
+         <testDataLinkId></testDataLinkId>
+         <type>DEFAULT</type>
+         <value></value>
+         <variableId>d0818746-8ff8-49bd-8e46-989ea62be5fc</variableId>
+      </variableLink>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>dfaa1cca-221f-4904-977f-bc76231ceb70</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Lexus.com/ModelPages/UXh/Offers/Enter Zip Code Unknown location - Enter Valid Zip code</testCaseId>
+      <variableLink>
+         <testDataLinkId></testDataLinkId>
+         <type>DEFAULT</type>
+         <value></value>
+         <variableId>3e5ca89c-1182-41aa-8954-0e8b1d311fa6</variableId>
+      </variableLink>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>95c3e7c0-8392-419d-9629-0b8ad346d0f3</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Lexus.com/ModelPages/UXh/Offers/Enter Zip Code Unknown location - Expand Zipcode</testCaseId>
+      <variableLink>
+         <testDataLinkId></testDataLinkId>
+         <type>DEFAULT</type>
+         <value></value>
+         <variableId>8f2702ad-c964-42c8-80c6-13246b1d6ff1</variableId>
+      </variableLink>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>7f58cde1-5f65-4e45-a784-44f65ce8aef7</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Lexus.com/ModelPages/UXh/Offers/Enter Zip Code Unknown location - Invalid Zip entry</testCaseId>
+      <variableLink>
+         <testDataLinkId></testDataLinkId>
+         <type>DEFAULT</type>
+         <value></value>
+         <variableId>ef1fa6af-5f20-46d7-8373-4319b7268a72</variableId>
+      </variableLink>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>3a051f51-50a2-42f8-922a-95db8d6d38a4</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Lexus.com/ModelPages/UXh/Offers/Enter Zip Code Unknown location - Non-Continental US Zip code Entry</testCaseId>
+      <variableLink>
+         <testDataLinkId></testDataLinkId>
+         <type>DEFAULT</type>
+         <value></value>
+         <variableId>ec7d76eb-3a00-449a-b9cb-45c8a90c6dca</variableId>
+      </variableLink>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>0e447b67-cfc1-4481-9464-db97fe19b325</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Lexus.com/ModelPages/UXh/Offers/Enter Zip Code Unknown location - Shared Market Zip Entry</testCaseId>
+      <variableLink>
+         <testDataLinkId></testDataLinkId>
+         <type>DEFAULT</type>
+         <value></value>
+         <variableId>44be82ca-5c33-473b-91a0-06401c8f6738</variableId>
+      </variableLink>
+   </testCaseLink>
+</TestSuiteEntity>
---
+++ Test Suites/Lexus.com/ModelPages/UXh/Overlay.ts
@@ -0,0 +1,26 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Overlay</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>15</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>f217950d-1ce7-4415-b8dc-cc9b0f3a79e7</testSuiteGuid>
+   <testCaseLink>
+      <guid>e5bdad17-a3a5-402e-9fd7-7a069b39a2e2</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Lexus.com/ModelPages/UXh/Overlay/Correct MSRP values in overlay</testCaseId>
+      <variableLink>
+         <testDataLinkId></testDataLinkId>
+         <type>DEFAULT</type>
+         <value></value>
+         <variableId>6c88e09d-4a36-4305-b28b-b45ea5b4c208</variableId>
+      </variableLink>
+   </testCaseLink>
+</TestSuiteEntity>
