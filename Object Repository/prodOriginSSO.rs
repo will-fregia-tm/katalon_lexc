7c83397e0499d9cec3f7482621489524c9656a2d
zsmith <zsmith@USLAXSSF9087.global.publicisgroupe.net>
2020-10-10 10:08:55 -0700

    SSO cookie updates

---
+++ Object Repository/prodOriginSSO.rs
@@ -0,0 +1,47 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebServiceRequestEntity>
+   <description></description>
+   <name>prodOriginSSO</name>
+   <tag></tag>
+   <elementGuidId>f232b5a7-b0b2-420b-a7fd-807aa88862a6</elementGuidId>
+   <selectorMethod>BASIC</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+   <connectionTimeout>0</connectionTimeout>
+   <followRedirects>false</followRedirects>
+   <httpBody></httpBody>
+   <httpBodyContent></httpBodyContent>
+   <httpBodyType></httpBodyType>
+   <httpHeaderProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>Cookie</name>
+      <type>Main</type>
+      <value>AWSELBAuthSessionCookie-0=oStC4w2y2iQ6tY76yffT6oMAsSAvH7c/7pLGjGS2GDalfptdxEsdFkC1Bm86IjWDskbGWCQeocR5V8jRyBvLODLgUbF1SHkxaFTn2A8LWgj9Zyv6SGcNtiWFNWB2UR2frRNfg0EMNvXLwckWZXJjhDQLY2MbybdcKPoPKTL67zTOJZ/5zd7poS9dq+4t+sm48qy5P9WgXoWvOuQdkxG1wHJumRVqfOG8QB3vImqvoqJMqdAEkO3AWIEp93anlezSkR164csn9aaosoru3r1/orsdwbbpKHTOLc/IioyHYvSZEfui5SPFTE9vOnb3H1V9GctsN5lsoKbkcZmx9sZkRDOrkb3ZgJTCfKFjh0NhpzZY1jOyTAnv5NSLFJqijJBaio3643Cl9Q6FWktagDiH0qIL9ETokM7h72byXy9T9keLxrS1TPrmEkKRFtzj3td+2mS4Bb/MnXMz3wnDYYsEixwFcsSCWrXFZYXTdmdMYlbNavGJDuyC8I+grBXY+paft/4pLcIaN7KY89LK+4MYkJDeNT9SFk5RTAjXHjt8gbnpGfIuRRUU6d2ImG0cM7fc+MlxXxAL9DTgwG+ByVMVIVBDglghj6EZzpSUDuhbUBWaCpodr8wpXfBomyH1TEwczqgfubLsd7Zk4vLFEjJRIKuTD0QESEwfm++kQWVxQw1X5wQSsGu7yAILLACUPnRgOvltbol7zU9ThYvldjmKyoTfJOp6iqPWGpluOu8ymeWYzb/jDp2R6fuW54lApuIGmaeYxRSfZrpfbNHc/6F/clAng1wt+2rstrO+52LEN9b+1BDIBwzMJ5YtVUzcMiozMMcW4oFCjdyl23ZO+Ji7ZRuMcDMjrOPYrrMuj0IefQp94t5AYc9rjpQmETOMSVJ0oip+D4DNyoYNpjYABaqBllNnA2bWgudUIoVQGZrWmbU+MM2dqNAgQPmCtrP3cGttYOCCjrTJzTLklmL0szCqx3Of1jqcR8cg2pVXhM/PO6dqhU7FoqvLACJ2x4LYW4nlrIid+wV0Mr59oEqwGnS7PAG9Izvg6hxjmkNoHefNYovFunKfDAluaeKaL6FpMJDf0JYPUM/x41iB/Enc721s4oVhTzrcD8eJ3nGs4+oS2XeWN1gCAMq/DDu9ounVKO4Iu+LUsHOYuemC3SnvjXqE2P8Od4RC3/iLwG59Uevqi0wEhlmeYJTlwRPr3xoPFnyFu++DKHuXeAjpw9q7i2BjuAuwyEMUS/hdutBVRjmr2pnk1sNTt6a1TTTknrsWiyqy7G9uhgDfY9ryXqG4swez/OSokDMDJXrx7QZl6zWmAFfB1zjwsQoft5IXnqOKy28m4lgjmTxo67wCnw9bxyDZI7TYxMHRxtyvMEkEFRZR3H/HWAqdVBgYiJ7tNX/aibfzN7JjjVN7Qe/cgCXZIYcYjPS8jiD4m6Gi99iMloVA//j5pvSuVf0crYy9r8LClEMim8glVbTBE490OTjHNB6BIaciI+uPAYUXelPiiYKwVtSxInEzlzec+yQ9BoBPVcId/NKfHM97+cH97tPhR3qGK+N3GpAEHrvWtBei/J22CZFSzasVztPqmJOIiuNqnAvPiTB1k1HXSu8ZUwUMiZ4JYEW5M2YSEHEeRh9Mki4rY3wmbmZO/QsyyrcQD+oBjhwaOriAE+Ti0B/X6Qh8093H29a1PT/DcdDd4OMl16ckcsT0uiy28QDnUjwys9msTyEDgXJZryg7f2qoWmcyRpJQDAikzyyl8XlO7Rw4xZnbMFZs4+lZGNd2mS5KC9hJRv0ALrk08c6KGZM6msuFP6n4+OAlPpgtlRx3+4uAMkDRPv2Y03Pon6mGMPtvxfVQdVNUvMCcahTEUjzdEEeUI598kZ/ioKNI5f5fyAuGaUcodL5zLdU13sG/7nOGR2yt8r1OTVriGBV3MtKQjy76AUI3JXRPAhO3hWZNR4ifhG044oTWdHepalG0dzA+JkiSuXIaacTbIQSjB0pbsPwLjjml28KLrvP64YpD9yxE2ZpkvjgL2LnerlGcdZxZE+7UnV3GdN5bEmPOFE04BmwjEz18wahX5cEMB0VwOTpFlGaZlSP52OJrp1pVCGGr0btEzKBECWlhl5cB340QJ/bbxxFExxd7gl3DyJhXUIKzjouatdpOGGLntoyN6SfE+ctOJLQN62mAt1XkaplulVtYovTyRBCCE3+6aZCK+V4/6721LYQH9iCthfpttMiSGlW42p5NuquYJsSqMEt5fVwqW4czabWI2xz6FCbC95Z2qjVVqV8RDg0l8uUZNjQSNn8g+HBphxhPe+0DjgS5xUg+SYHBsK/de6GfZ6WNoxfCkVlpxttfSGd+o+iuMfz6oSPCsXKfqR+qPvOoEReOIb9XDw6cCLem7kNP8sfWYB5kRWK0oK61yVQSwQ/OwP64bgiDL+VhYdm1izLAaVKWYM0A6ND/pIz0yJpVsXZBkkHf+F1ieZd02VspLvunTOfkxd0i9QC7ED91Hk28phMfoQXGgZdDpqTJwtk50cPj8qc5nL5jHH3lGvGDlL1PU/NRTYbaBNLnKtjmLY5ss/bO6KtEz+yThHo6THo6E/gNyTWhwfWIqSKUS8jqNT0kbLVg15PZMWIrTObHbEzaCgyC0GQDV7bebiyKUatw1lGJK4hCjIW4KDRbH09pPz+C+zQj8GqrJYf7yp9nY7EOprOlCEeN1r5DPvluquIxdfPkHN8/PeFip9sAeGbww2tAs3W5aH2Q7FnbTtnPIlhpBAmgUUspE0TI68IUZx79xKkYSbyxdhjqoJakaGMu4IUUT5kocr4oB+x8iKXaSY4GOWXn6RCqLjjKG4/HbTYp781ikUvjUhau613oGmhb0w4r+hljHBDwpTgBXaUMSR0NMwTSWd0bPCvoRs/H4SzI7Opx8TzNJPtAB8nsmUPzig2m5zrw3CTdz5wO4qYtcNhbfzvCdI/tRUSiQtiM3VDfjgR5Z/c/5T5yJOhJ55gzhISQRW91kBzVOLZemc/vr3nY5jPHql6zTyFf8fB/ae65hSCw2Sk+WGM85Hf4Dkzz82Omg6fqJvotpPS99FxkKhfzeryazuPyQ8YYf8l+WWj3G8sIhr1X8/IGOy6NDrrEIjH1YspTvwkBfna8WJIGPGzc6vLDiOMpwrn+C0+RsuNiAsWiQSx68J7M+tyQwtDizpy6JlNh92g5zS+RfOcsd/N2nwvY2L2YG+fWeRehCid0mIjYoLDHLuXfsiOwiW3swUJzH0MaBZu6cqvY+A/6Q1WVDPmnLtRatUNIN+TpinYINIgAmp1gJyqKtQwgjxBLTx9d2g08P6L7OTdfpaYNnw==</value>
+   </httpHeaderProperties>
+   <maxResponseSize>0</maxResponseSize>
+   <migratedVersion>5.4.1</migratedVersion>
+   <restRequestMethod>GET</restRequestMethod>
+   <restUrl>https://prd-aem-lcom.origin.cepo-proxy.tms.aws.lexus.com/concept/LFSA</restUrl>
+   <serviceType>RESTful</serviceType>
+   <soapBody></soapBody>
+   <soapHeader></soapHeader>
+   <soapRequestMethod></soapRequestMethod>
+   <soapServiceEndpoint></soapServiceEndpoint>
+   <soapServiceFunction></soapServiceFunction>
+   <socketTimeout>0</socketTimeout>
+   <useServiceInfoFromWsdl>true</useServiceInfoFromWsdl>
+   <verificationScript>import static org.assertj.core.api.Assertions.*
+
+import com.kms.katalon.core.testobject.RequestObject
+import com.kms.katalon.core.testobject.ResponseObject
+import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
+import com.kms.katalon.core.webservice.verification.WSResponseManager
+
+import groovy.json.JsonSlurper
+import internal.GlobalVariable as GlobalVariable
+
+RequestObject request = WSResponseManager.getInstance().getCurrentRequest()
+
+ResponseObject response = WSResponseManager.getInstance().getCurrentResponse()</verificationScript>
+   <wsdlAddress></wsdlAddress>
+</WebServiceRequestEntity>
