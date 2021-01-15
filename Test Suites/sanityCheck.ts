90b5a5ee4c5fc4002a77f61a99fe6e05f9a7e384
zsmith <zsmith@USLAXSSF9087.global.publicisgroupe.net>
2021-01-15 11:14:15 -0800

    iOS14, android 10 added

--- Test Suites/sanityCheck.ts
+++ Test Suites/sanityCheck.ts
@@ -73,7 +73,7 @@
          <configuration>
             <groupName>Custom</groupName>
             <profileName>staging</profileName>
-            <runConfigurationId>saucelabs_chrome_android8_galaxys8</runConfigurationId>
+            <runConfigurationId>saucelabs_chrome_android10_pixel3</runConfigurationId>
          </configuration>
          <runEnabled>true</runEnabled>
          <testSuiteEntity>Test Suites/Lexus.com/FAD/mobile tests</testSuiteEntity>
