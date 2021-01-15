f5a7e2eb3228e9f26eb1a0ec03618f5865c611a8
zsmith <zsmith@USLAXSSF9087.global.publicisgroupe.net>
2021-01-14 17:00:22 -0800

    removed gallery heading h2 tag to accept h3 on dev

--- Object Repository/FAD/DealerDetails/gallery heading.rs
+++ Object Repository/FAD/DealerDetails/gallery heading.rs
@@ -7,13 +7,13 @@
    <selectorCollection>
       <entry>
          <key>BASIC</key>
-         <value>//h2[(contains(text(), 'GALLERY') or contains(., 'GALLERY'))]</value>
+         <value>//*[(contains(text(), 'GALLERY') or contains(., 'GALLERY'))]</value>
       </entry>
    </selectorCollection>
    <selectorMethod>BASIC</selectorMethod>
    <useRalativeImagePath>false</useRalativeImagePath>
    <webElementProperties>
-      <isSelected>true</isSelected>
+      <isSelected>false</isSelected>
       <matchCondition>equals</matchCondition>
       <name>tag</name>
       <type>Main</type>
