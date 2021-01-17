d98dcd77781c1ef7d610b2c1d601e3820f3612d6
zsmith <zsmith@USLAXSSF9087.global.publicisgroupe.net>
2021-01-17 10:40:28 -0800

    disabled staging compare report tests

--- Test Suites/sanityCheck.ts
+++ Test Suites/sanityCheck.ts
@@ -201,7 +201,7 @@
             <profileName>staging</profileName>
             <runConfigurationId>saucelabs_chrome_win10</runConfigurationId>
          </configuration>
-         <runEnabled>true</runEnabled>
+         <runEnabled>false</runEnabled>
          <testSuiteEntity>Test Suites/Lexus.com/MSRP/CompareReport</testSuiteEntity>
       </TestSuiteRunConfiguration>
       <TestSuiteRunConfiguration>
