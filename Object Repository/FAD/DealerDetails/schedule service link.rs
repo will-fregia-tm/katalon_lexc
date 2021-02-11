01fcd4ca362f00df316669a460c6e61ae1931e1d
zsmith <zsmith@USLAXSSF9087.global.publicisgroupe.net>
2021-02-11 12:14:55 -0800

    link xpath text update

--- Object Repository/FAD/DealerDetails/schedule service link.rs
+++ Object Repository/FAD/DealerDetails/schedule service link.rs
@@ -6,14 +6,18 @@
    <elementGuidId>597e58c5-369a-4205-a2d6-4d196800dfc9</elementGuidId>
    <selectorCollection>
       <entry>
-         <key>CSS</key>
-         <value>a[text*=&quot;SCHEDULE SERVICE&quot;][target*=&quot;_blank&quot;]</value>
-      </entry>
-      <entry>
          <key>BASIC</key>
          <value></value>
       </entry>
+      <entry>
+         <key>XPATH</key>
+         <value>//a[@target = '_blank' and (contains(text(), 'SCHEDULE SERVICE') or contains(., 'SCHEDULE SERVICE'))]</value>
+      </entry>
+      <entry>
+         <key>CSS</key>
+         <value>a[text*=&quot;SCHEDULE SERVICE&quot;][target*=&quot;_blank&quot;]</value>
+      </entry>
    </selectorCollection>
-   <selectorMethod>CSS</selectorMethod>
+   <selectorMethod>XPATH</selectorMethod>
    <useRalativeImagePath>false</useRalativeImagePath>
 </WebElementEntity>
