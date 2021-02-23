89a26c75db043a113a65381c10370dc0a59ecb0e
zsmith <zsmith@USLAXSSF9087.global.publicisgroupe.net>
2021-02-23 10:34:13 -0800

    more debugging

---
+++ Object Repository/Homepage/HeroModule/spec - starting at msrp 2.rs
@@ -0,0 +1,18 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>spec - starting at msrp 2</name>
+   <tag></tag>
+   <elementGuidId>8a5c7187-110a-4605-9305-c3b0d5f818c6</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>XPATH</key>
+         <value>(//*[@id=&quot;NonSEHero&quot;]//div[(contains(text(), 'STARTING AT') or contains(., 'STARTING AT'))]/following-sibling::span)[2]</value>
+      </entry>
+      <entry>
+         <key>BASIC</key>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+</WebElementEntity>
