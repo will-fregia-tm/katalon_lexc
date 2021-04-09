1e761be5e033cc22ef9522ad463edccaf86d9763
zsmith <zsmith@USLAXSSF9087.global.publicisgroupe.net>
2021-04-09 13:08:04 -0700

    debugging

--- Object Repository/FCV/Hero/social media overlay.rs
+++ Object Repository/FCV/Hero/social media overlay.rs
@@ -6,11 +6,15 @@
    <elementGuidId>978f0d83-0044-49f0-86bc-847d4d858d5d</elementGuidId>
    <selectorCollection>
       <entry>
+         <key>XPATH</key>
+         <value>//button[@aria-label = 'Facebook']/parent::div</value>
+      </entry>
+      <entry>
          <key>BASIC</key>
          <value>//*[@id = 'SubPageOverlayScroll']</value>
       </entry>
    </selectorCollection>
-   <selectorMethod>BASIC</selectorMethod>
+   <selectorMethod>XPATH</selectorMethod>
    <useRalativeImagePath>false</useRalativeImagePath>
    <webElementProperties>
       <isSelected>true</isSelected>
