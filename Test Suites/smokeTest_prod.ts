1f6f61ad9c02f5505d4de0a97d890b3d0c7ef997
zach-smith-tm <zach.smith@toyota.com>
2021-06-24 17:44:32 -0700

    legacy url update

--- Test Suites/smokeTest_prod.ts
+++ Test Suites/smokeTest_prod.ts
@@ -46,7 +46,7 @@
          <configuration>
             <groupName>Custom</groupName>
             <profileName>prod</profileName>
-            <runConfigurationId>saucelabs_firefox_win10</runConfigurationId>
+            <runConfigurationId>saucelabs_edge_win10</runConfigurationId>
          </configuration>
          <runEnabled>true</runEnabled>
          <testSuiteEntity>Test Suites/Lexus.com/all smoke test suites/RX</testSuiteEntity>
